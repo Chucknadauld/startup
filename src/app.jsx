a90596e4f04b57eb69fd9cba189ac810cbd6328b
Chuck Nadauld <charlienadauld@gmail.com>
2025-10-08 23:40:04 -0600

    Add header and footer to App component

--- src/app.jsx
+++ src/app.jsx
@@ -3,5 +3,33 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
 export default function App() {
-  return <div className="body bg-dark text-light">App will display here</div>;
+  return (
+    <div className="body bg-dark text-light">
+      <header>
+        <nav>
+          <div>
+            <h1>BeatQueue</h1>
+            <p>Real-Time Collaborative Playlists for DJs</p>
+          </div>
+          <div>
+            <a href="index.html">Home</a>
+            <a href="dashboard.html">Dashboard</a>
+            <a href="event.html">Live Event</a>
+            <a href="join.html">Join Event</a>
+          </div>
+        </nav>
+      </header>
+
+      <main>App components go here</main>
+
+      <footer>
+        <div>
+          <p>&copy; 2025 BeatQueue - Built by Chuck Nadauld</p>
+          <p>
+            <a href="https://github.com/Chucknadauld/startup">View on GitHub</a>
+          </p>
+        </div>
+      </footer>
+    </div>
+  );
 }
