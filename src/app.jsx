bdc48f5e56e56b977e9b68400d88b5b22059dca8
Chuck Nadauld <charlienadauld@gmail.com>
2025-10-08 23:43:05 -0600

    Set up React router with navigation

--- src/app.jsx
+++ src/app.jsx
@@ -1,35 +1,52 @@
 import React from 'react';
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { Login } from './login/login';
+import { Dashboard } from './dashboard/dashboard';
+import { Event } from './event/event';
+import { Join } from './join/join';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
 export default function App() {
   return (
-    <div className="body bg-dark text-light">
-      <header>
-        <nav>
-          <div>
-            <h1>BeatQueue</h1>
-            <p>Real-Time Collaborative Playlists for DJs</p>
-          </div>
-          <div>
-            <a href="index.html">Home</a>
-            <a href="dashboard.html">Dashboard</a>
-            <a href="event.html">Live Event</a>
-            <a href="join.html">Join Event</a>
-          </div>
-        </nav>
-      </header>
+    <BrowserRouter>
+      <div className="body bg-dark text-light">
+        <header>
+          <nav>
+            <div>
+              <h1>BeatQueue</h1>
+              <p>Real-Time Collaborative Playlists for DJs</p>
+            </div>
+            <div>
+              <NavLink to="">Home</NavLink>
+              <NavLink to="dashboard">Dashboard</NavLink>
+              <NavLink to="event">Live Event</NavLink>
+              <NavLink to="join">Join Event</NavLink>
+            </div>
+          </nav>
+        </header>
 
-      <main>App components go here</main>
+        <Routes>
+          <Route path='/' element={<Login />} exact />
+          <Route path='/dashboard' element={<Dashboard />} />
+          <Route path='/event' element={<Event />} />
+          <Route path='/join' element={<Join />} />
+          <Route path='*' element={<NotFound />} />
+        </Routes>
 
-      <footer>
-        <div>
-          <p>&copy; 2025 BeatQueue - Built by Chuck Nadauld</p>
-          <p>
-            <a href="https://github.com/Chucknadauld/startup">View on GitHub</a>
-          </p>
-        </div>
-      </footer>
-    </div>
+        <footer>
+          <div>
+            <p>&copy; 2025 BeatQueue - Built by Chuck Nadauld</p>
+            <p>
+              <a href="https://github.com/Chucknadauld/startup">View on GitHub</a>
+            </p>
+          </div>
+        </footer>
+      </div>
+    </BrowserRouter>
   );
+}
+
+function NotFound() {
+  return <main className="container-fluid bg-secondary text-center">404: Return to sender. Address unknown.</main>;
 }
