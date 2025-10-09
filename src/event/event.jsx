1281b34bd5f9495e6cebfbfbf771a8c1d4618d06
Chuck Nadauld <charlienadauld@gmail.com>
2025-10-08 23:41:58 -0600

    Create stub components for all views

---
+++ src/event/event.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Event() {
+  return (
+    <main className="container-fluid bg-secondary text-center">
+      <div>event displayed here</div>
+    </main>
+  );
+}
